7592872ce649dd216ee2933bb796ce5a42baff1f
Ejinks <ejinks2@stevens.edu>
2025-04-04 11:37:15 -0400

    Incorportating more tailwindcss styling.

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ function App() {
 
   return (
     <>
-        <div className="w-auto h-full bg-white rounded-lg">
-            <div>
+        <div>
+            <div className='w-1/2 h-30 bg-white rounded-lg flex flex-col items-center justify-center shadow-xl'>
                 <h1 className="text-4xl block">Ethan Jinks</h1>
                 <h2 className='block'>Welcome to my portfolio!</h2>
             </div>
             <br />
-            <div>
-                <h2>About Me</h2>
-                <h3>
+            <div className="w-2/3 h-60 bg-white rounded-lg shadow-xl ml-auto p-5">
+                <h2 className="text-4xl text-left">About Me</h2>
+                <h3 className='p-5'>
                 Hi! My name is Ethan Jinks. I'm a junior Computer Science major at Stevens
                 Institute of Technology with a passion for learning and problem solving!
                 Some of my favorite courses that I've taken include Systems Programming,
@@ -21,31 +21,39 @@ function App() {
                 used to help others!
                 </h3>
             </div>
-            <div className="">
-                <h2 className="text-2xl">Projects</h2>
+            <div className="w-full h-4/5 bg-white rounded-lg shadow-xl mt-5 p-5">
+                <h2 className="text-4xl text-left">Projects</h2>
+
+                <div className="w-full h-px bg-[#34373A] mt-1" ></div>
+                <div className="w-full h-px bg-[#34373A] mt-1" ></div>
+                
                 <ul>
                 <li>
-                    <h3 className="text-xl">Simulated Recycling Bin</h3>
-                    <h4>
+                    <h3 className="text-xl text-left pt-3">Simulated Recycling Bin</h3>
+                    <h4 className='mt-2'>
                     Using the Bash interpreter language, I created a script that when the
                     user calls the script, creates a .recycle folder in the home directory
                     (if it does not exist) and when the user gives certain flags, either
                     moves files into the recycling folder or deletes all files inside the
                     recycling folder, simulating a recycling bin.
                     </h4>
-                    <a href="https://github.com/Ejinks2/recycling" target="_blank" rel="noreferrer">
+                    <a href="https://github.com/Ejinks2/recycling" target="_blank" rel="noreferrer" className="text-xl">
                     Simulated Recycling Bin
                     </a>
                 </li>
                 <li>
-                    <h3 className="text-xl antialiased">Shell Script</h3>
-                    <h4>
+                    <div className="w-full h-px bg-[#34373A] mt-1" ></div>
+                    <div className="w-full h-px bg-[#34373A] mt-1" ></div>
+                </li>
+                <li>
+                    <h3 className="text-xl text-left pt-3">Shell Script</h3>
+                    <h4 className='mt-2'>
                     Using the C language, I was able to build a shell script that read
                     user input from the terminal and then executed different commands
                     depending on user input. I gained skills concerning processes and
                     forking, tokens, buffers, and common C macros.
                     </h4>
-                    <a href="https://github.com/Ejinks2/ShellScript" target="_blank" rel="noreferrer">
+                    <a href="https://github.com/Ejinks2/ShellScript" target="_blank" rel="noreferrer" className='text-xl'>
                     Shell Script
                     </a>
                 </li>
@@ -61,7 +69,7 @@ function App() {
             </div>
         </div>
     </>
-  )
+  );
 }
 
 export default App
